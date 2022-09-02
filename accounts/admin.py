00aed4a12cc523c0626c41b576f421ea68888933
Alexis Pieropan <ale-210@live.com>
2022-09-01 23:48:04 -0300

    modificado

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'address', 'dni')
+    fieldsets = (
+        *UserAdmin.fieldsets,  
+        (                      
+            'Información adicional',  
+            {
+                'fields': (
+                    'address',
+                    'dni',
+                    'role',
+                )
+            }
+        )           )
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
